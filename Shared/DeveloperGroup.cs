a98515d0300a674355854015fc4401f0a0c072a4
raulglezrdguez <raulglezrdguez69@gmail.com>
2022-08-31 15:11:15 -0400

    Customer and DeveloperGroup class created

--- Shared/DeveloperGroup.cs
+++ Shared/DeveloperGroup.cs
@@ -2,5 +2,7 @@ namespace DeveloperPlace.Shared;
 
 public class DeveloperGroup
 {
-
+    public Customer Customer { get; set; } = new Customer();
+    public List<int> Members { get; set; } = new List<int>();
+    public bool GroupClosed { get; set; } = false;
 }

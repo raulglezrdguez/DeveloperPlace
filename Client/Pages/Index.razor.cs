7efc3b54355a29287ffb4e13a9b6275981c19d3a
raulglezrdguez <raulglezrdguez69@gmail.com>
2022-09-02 08:05:58 -0400

    Index code created

---
+++ Client/Pages/Index.razor.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components;
+using DeveloperPlace.Shared;
+
+namespace DeveloperPlace.Client;
+
+
+public partial class Index : ComponentBase
+{
+    private State State { get; } = new State();
+
+    protected override void OnInitialized()
+    {
+        State.DeveloperList.Add(new Developer(1, "Peter", 100, Levels.Trainee));
+        State.DeveloperList.Add(new Developer(2, "Susan", 200, Levels.Junior));
+        State.DeveloperList.Add(new Developer(3, "Ana", 300, Levels.Mid_level));
+        State.DeveloperList.Add(new Developer(4, "Carl", 400, Levels.Senior));
+        State.DeveloperList.Add(new Developer(5, "Elena", 500, Levels.Leader));
+    }
+}

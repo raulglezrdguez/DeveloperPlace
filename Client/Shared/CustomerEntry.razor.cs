32a935e7001ec4e38f6e6f1fb4ddc82e70fdc392
raulglezrdguez <raulglezrdguez69@gmail.com>
2022-09-04 10:50:01 -0400

    CustomerEntry component created

---
+++ Client/Shared/CustomerEntry.razor.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using DeveloperPlace.Shared;
+
+namespace DeveloperPlace.Client.Shared
+{
+    public partial class CustomerEntry
+    {
+        [Parameter]
+        public string Title { get; set; } = default!;
+
+        [Parameter]
+        public string ButtonTitle { get; set; } = default!;
+
+        [Parameter]
+        public string ButtonClass { get; set; } = default!;
+
+        [Parameter]
+        public Customer Customer { get; set; } = default!;
+
+        [Parameter]
+        public EventCallback ValidSubmit { get; set; } = default!;
+    }
+}
